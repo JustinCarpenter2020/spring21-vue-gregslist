8d53e6e2c908da8babd90693ca81e7193e2d6981
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-04-27 17:58:06 -0600

    done

--- src/components/CarComponent.vue
+++ src/components/CarComponent.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="car col-md-4 mb-3">
+  <div class="card col-md-4 mb-3" data-toggle="modal" :data-target="'#displayModal' + car.id">
     <!-- Route parameter getting set with the Params object -->
-    <router-link :to="{name: 'CarDetails', params: { id: car.id }}">
-      <div class="card shadow">
-        <img class="card-img-top" :src="car.imgUrl" alt="">
-        <div class="card-body">
-          <h4 class="card-title">
-            {{ car.make }} - {{ car.model }}
-          </h4>
-        </div>
+    <!-- <router-link :to="{name: 'CarDetails', params: { id: car.id }}"> -->
+    <div class="card shadow">
+      <img class="card-img-top" :src="car.imgUrl" alt="">
+      <div class="card-body">
+        <h4 class="card-title">
+          {{ car.make }} - {{ car.model }}
+        </h4>
       </div>
-    </router-link>
+    </div>
+    <!-- </router-link> -->
   </div>
+  <DisplayModalComponent :car-prop="car">
+    <template #title>
+      <h5>{{ car.make }}</h5>
+      <p>hello form the parent</p>
+    </template>
+  </DisplayModalComponent>
 </template>
 
 <script>
+import DisplayModalComponent from './DisplayModalComponent'
 export default {
   name: 'Car',
   props: {
@@ -26,7 +33,7 @@ export default {
   setup() {
     return {}
   },
-  components: {}
+  components: { DisplayModalComponent }
 }
 </script>
 
---
+++ src/components/DisplayModalComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- NOTE make sure this matches with the data target on the parent no hashtag and must be unique! -->
+  <div class="modal fade"
+       :id="'displayModal' + carProp.id"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="exampleModalLabel"
+       aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <!-- NOTE slots alow for data to be injected -->
+          <slot name="title" class="modal-title" id="exampleModalLabel">
+            <p>fill out title template</p>
+          </slot>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row text-center">
+            <div class="col-6">
+              <p>Make: {{ carProp.make }}</p>
+            </div>
+            <div class="col-6">
+              <p>Model: {{ carProp.model }}</p>
+            </div>
+          </div>
+          <div class="row">
+            <slot name="pic" class="col-12">
+              <img :src="carProp.imgUrl" alt="" class="img-fluid">
+            </slot>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              {{ carProp.description }}
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">
+            Save changes
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayModalComponent',
+  props: {
+    carProp: { type: Object, required: true }
+  },
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
